00f284fda0ec13905b206b2fff167b01baa99095
freesaber <free.saber@qq.com>
2021-01-27 16:45:53 +0800

    webapi，获取授权

---
+++ 06-etc/01-IdentityServer4/yidingtong/Sample/Sample.WebApi/Controllers/IdentityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.WebApi.Controllers
+{
+    [ApiController]
+    [Route("identity")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly ILogger<IdentityController> _logger;
+
+        public IdentityController(ILogger<IdentityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return new JsonResult(
+                from claim in User.Claims
+                select new
+                {
+                    claim.Type,
+                    claim.Value
+                }
+            );
+        }
+    }
+}
